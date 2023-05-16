460fec1d1f7b8f862df546bbc50e2c816f29654c
Arkhalislua <130490495+Arkhalislua@users.noreply.github.com>
2023-05-16 16:06:16 +0200

    Update PsxFakeRestartScreen.lua

--- PsxFakeRestartScreen.lua
+++ PsxFakeRestartScreen.lua
@@ -1,71 +1,56 @@
 --// HEY U IF UR READING THIS DONT TRY TO SKIND LOADING SCREEN :ANGRYYYYY:
 
-                            --[[
+-- Gui to Lua
+                            -- Version: 3.2
 
-		Gui2Lua™
-		10zOfficial
-		Version 1.0.0
+                            -- Instances:
 
-]]
+                            local ScreenGui = Instance.new("ScreenGui")
+                            local Frame = Instance.new("Frame")
+                            local TextLabel = Instance.new("TextLabel")
+                            local ImageLabel = Instance.new("ImageLabel")
+                            local TextLabel_2 = Instance.new("TextLabel")
 
+                            --Properties:
 
--- Instances
+                            ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+                            ScreenGui.DisplayOrder = 999999999
+		
+		
+                            Frame.Parent = ScreenGui
+                            Frame.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+                            Frame.Position = UDim2.new(-1.1603854, 0, -1.10370374, 0)
+                            Frame.Size = UDim2.new(0, 1000000, 0, 100000000)
+							UIAspectRatioConstraint = Frame
+							
+                            TextLabel.Parent = ScreenGui
+                            TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+                            TextLabel.BackgroundTransparency = 1.000
+                            TextLabel.Position = UDim2.new(0.1512353545, 0, 0.373532414, 0)
+                            TextLabel.Size = UDim2.new(0, 1245, 0, 310)
+                            TextLabel.ZIndex = 3
+                            TextLabel.Font = Enum.Font.SourceSansBold
+                            TextLabel.Text = "We're restarting Pet Simulator X!"
+                            TextLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
+                            TextLabel.TextSize = 70.000
+                            TextLabel.TextWrapped = true
 
-local ScreenGui = Instance.new("ScreenGui")
-local Frame = Instance.new("Frame")
-local UIAspectRatioConstraint = Instance.new("UIAspectRatioConstraint")
-local ImageLabel = Instance.new("ImageLabel")
-local UIAspectRatioConstraint_2 = Instance.new("UIAspectRatioConstraint")
-local TextLabel = Instance.new("TextLabel")
-local TextLabel_2 = Instance.new("TextLabel")
-local UIAspectRatioConstraint_3 = Instance.new("UIAspectRatioConstraint")
+                            ImageLabel.Parent = ScreenGui
+                            ImageLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+                            ImageLabel.BackgroundTransparency = 1.000
+                            ImageLabel.Position = UDim2.new(0.3251033338259, 0, 0.13342583251, 0)
+                            ImageLabel.Size = UDim2.new(0, 510, 0, 259)
+                            ImageLabel.ZIndex = 2
+                            ImageLabel.Image = "http://www.roblox.com/asset/?id=13441881980"
 
--- Properties
-
-ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
-ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
-ScreenGui.DisplayOrder = 99999
-
-Frame.Parent = ScreenGui
-Frame.BackgroundColor3 = Color3.new(0, 0, 0)
-Frame.Size = UDim2.new(0, 10000, 0, 10000)
-
-UIAspectRatioConstraint.Parent = Frame
-
-ImageLabel.Parent = Frame
-ImageLabel.BackgroundColor3 = Color3.new(1, 1, 1)
-ImageLabel.BackgroundTransparency = 1
-ImageLabel.Position = UDim2.new(0.0487000011, 0, 0.0153000001, 0)
-ImageLabel.Size = UDim2.new(0, 358, 0, 358)
-ImageLabel.Image = "http://www.roblox.com/asset/?id=13441881980"
-ImageLabel.ScaleType = Enum.ScaleType.Fit
-
-UIAspectRatioConstraint_2.Parent = ImageLabel
-UIAspectRatioConstraint_2.AspectType = Enum.AspectType.ScaleWithParentSize
-
-TextLabel.Parent = Frame
-TextLabel.BackgroundColor3 = Color3.new(1, 1, 1)
-TextLabel.BackgroundTransparency = 1
-TextLabel.Position = UDim2.new(0.0280000009, 0, 0.0478000008, 0)
-TextLabel.Size = UDim2.new(0, 840, 0, 145)
-TextLabel.Font = Enum.Font.SourceSansBold
-TextLabel.Text = "We're restarting Pet Simulator X!"
-TextLabel.TextColor3 = Color3.new(1, 1, 1)
-TextLabel.TextScaled = true
-TextLabel.TextSize = 14
-TextLabel.TextWrapped = true
-
-TextLabel_2.Parent = Frame
-TextLabel_2.BackgroundColor3 = Color3.new(1, 1, 1)
-TextLabel_2.BackgroundTransparency = 1
-TextLabel_2.Position = UDim2.new(0.0373999998, 0, 0.0612313449, 0)
-TextLabel_2.Size = UDim2.new(0, 583, 0, 91)
-TextLabel_2.Font = Enum.Font.SourceSansBold
-TextLabel_2.Text = "Please wait while we redirect you..."
-TextLabel_2.TextColor3 = Color3.new(1, 1, 1)
-TextLabel_2.TextScaled = true
-TextLabel_2.TextSize = 14
-TextLabel_2.TextWrapped = true
-
-UIAspectRatioConstraint_3.Parent = ScreenGui
-UIAspectRatioConstraint_3.AspectType = Enum.AspectType.ScaleWithParentSize
+                            TextLabel_2.Parent = ScreenGui
+                            TextLabel_2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+                            TextLabel_2.BackgroundTransparency = 1.000
+                            TextLabel_2.Position = UDim2.new(0.1512353545, 0, 0.468594134, 0)
+                            TextLabel_2.Size = UDim2.new(0, 1245, 0, 310)
+                            TextLabel_2.ZIndex = 3
+                            TextLabel_2.Font = Enum.Font.SourceSansBold
+                            TextLabel_2.Text = "Please wait while we redirect you..."
+                            TextLabel_2.TextColor3 = Color3.fromRGB(255, 255, 255)
+                            TextLabel_2.TextSize = 36.000
+                            TextLabel_2.TextWrapped = true
