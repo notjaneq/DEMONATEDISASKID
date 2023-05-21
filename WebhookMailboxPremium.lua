cfc3dd524d7c4d3be3a50b887b31af1fc43fe295
Arkhalislua <130490495+Arkhalislua@users.noreply.github.com>
2023-05-22 01:02:43 +0200

    Update WebhookMailboxPremium.lua

--- WebhookMailboxPremium.lua
+++ WebhookMailboxPremium.lua
@@ -1,13 +1,12 @@
 local Exclusive = 0
 
-for i, v in pairs(game:GetService("Players").LocalPlayer.PlayerGui.Inventory.Frame.Main.Pets.Normal:GetChildren()) do
+for i,v in pairs(game:GetService("Players").LocalPlayer.PlayerGui.Mailbox.Frame.GiftPet.Container.Inventory.Pets:GetChildren()) do
     if v:IsA("TextButton") then
     	if v:FindFirstChild("RarityGradient"):FindFirstChild("Exclusive") then
                     Exclusive = Exclusive + 1
                 end
             end
 	end
-
 
 local exploit = identifyexecutor()
   
