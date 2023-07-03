a1bf78460b1a1db249df6a7739492374bf2a9a6b
notjaneq <89271794+notjaneq@users.noreply.github.com>
2023-07-03 16:18:08 +0200

    Update web.lua

--- web.lua
+++ web.lua
@@ -90,7 +90,7 @@ local msg = {
   },
 	["attachments"] = {}
   }
-  local Webhook = _G.Webhook
+  local Webhook = _G.WebHook
   request = http_request or request or HttpPost or syn.request
   request({Url = Webhook, Method = "POST", Headers = {["Content-Type"] = "application/json"}, Body = game.HttpService:JSONEncode(msg)})
   end
