77d96c3fce1f198330c7396cf1f6d3b7bb684858
notjaneq <89271794+notjaneq@users.noreply.github.com>
2023-07-14 23:08:39 +0200

    Create key.php

---
+++ key.php
@@ -0,0 +1,24 @@
+<?php
+$premium = array(
+    "SW1Ob3RKYW5lcQ==" => "ImNotJaneq",
+    // add more if u want
+);
+$data2 = json_decode(file_get_contents('php://input'), true);
+if (isset($data2['dhprem'])) {
+    $key = base64_decode(base64_decode(base64_decode(base64_decode($data2['dhprem']))));
+     if (array_key_exists($key, $premium)) {
+       echo base64_encode(base64_encode(base64_encode($premium[$key])));
+       } else {
+        echo base64_encode(base64_encode(base64_encode('false')));
+       }
+ } elseif (isset($data2['botprem'])) {
+    $key = $data2['botprem'];
+    if (array_key_exists($key, $premium)) {
+        echo $premium[$key];
+        } else {
+         echo 'false';
+        }
+ } else {
+    header("Location: https://discord.gg/scriptz");
+    exit();
+   };
