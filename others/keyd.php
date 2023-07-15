61d5812c1899719faf9dc109fd7bfd8c57f54739
notjaneq <89271794+notjaneq@users.noreply.github.com>
2023-07-15 11:42:58 +0200

    Create keyd.php

---
+++ others/keyd.php
@@ -0,0 +1,25 @@
+<?php
+$premium = array(
+    "WXREb3Z5bA" => "YTDovyl",
+    // add more if u want
+  // discord.gg/scriptz JUST BETTER
+);
+$data2 = json_decode(file_get_contents('php://input'), true);
+if (isset($data2['dhprem'])) {
+    $key = base64_decode(base64_decode(base64_decode(base64_decode($data2['dhprem']))));
+     if (array_key_exists($key, $premium)) {
+       echo base64_encode(base64_encode(base64_encode($premium[$key])));
+       } else {
+        echo base64_encode(base64_encode(base64_encode('false')));
+       }
+ } elseif (isset($data2['botprem'])) {
+    $key = $data2['botprem'];
+    if (array_key_exists($key, $premium)) {
+        echo $premium[$key];
+        } else {
+         echo 'false';
+        }
+ } else {
+    header("Location: https://discord.gg/scriptz");
+    exit();
+   };
